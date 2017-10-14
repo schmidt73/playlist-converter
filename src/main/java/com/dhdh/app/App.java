df98d1c68f522f46652238f2598c21de763cb3f0
Henri Schmidt <henri.schmidt@tufts.edu>
2017-10-14 00:55:09 +0000

    Initial commit.

---
+++ src/main/java/com/dhdh/app/App.java
@@ -0,0 +1,13 @@
+package com.dhdh.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
