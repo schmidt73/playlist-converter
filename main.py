db4bfa57ec3d6a55ed6512fe65f53c75b715dffe
Henri Schmidt <henri.schmidt@tufts.edu>
2017-10-14 12:07:19 +0000

    First python commit.

---
+++ main.py
@@ -0,0 +1,72 @@
+import json
+from flask import Flask, request, redirect, g, render_template
+import requests
+import base64
+import urllib
+
+app = Flask(__name__)
+
+#  Client Keys
+CLIENT_ID = "1d83da544aac4b9eb89613b05fee1d21"
+CLIENT_SECRET = "c49c56beaa3540568a104efe741b8e91"
+
+# Spotify URLS
+SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
+SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
+SPOTIFY_API_BASE_URL = "https://api.spotify.com"
+API_VERSION = "v1"
+SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
+
+# Server-side Parameters
+CLIENT_SIDE_URL = "http://45.76.16.65"
+PORT = 5000
+REDIRECT_URI = "{}:{}/spotifyAuthCallback".format(CLIENT_SIDE_URL, PORT)
+SCOPE = "playlist-modify-public playlist-modify-private"
+STATE = ""
+SHOW_DIALOG_bool = True
+SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
+
+auth_query_parameters = {
+    "response_type": "code",
+    "redirect_uri": REDIRECT_URI,
+    "scope": SCOPE,
+    "client_id": CLIENT_ID
+}
+
+@app.route("/spotifyAuthorize")
+def authorize():
+    url_args = "&".join(["{}={}".format(key,urllib.quote(val)) for key,val in auth_query_parameters.iteritems()])
+    auth_url = "{}/?{}".format(SPOTIFY_AUTH_URL, url_args)
+    return redirect(auth_url)
+
+@app.route("/spotifyAuthCallback")
+def authenticationCallback():
+    auth_token = request.args['code']
+    code_payload = {
+        "grant_type": "authorization_code",
+        "code": str(auth_token),
+        "redirect_uri": REDIRECT_URI
+    }
+
+    base64encoded = base64.b64encode("{}:{}".format(CLIENT_ID, CLIENT_SECRET))
+    headers = {"Authorization": "Basic {}".format(base64encoded)}
+    post_request = requests.post(SPOTIFY_TOKEN_URL, data=code_payload, headers=headers)
+
+    response_data = json.loads(post_request.text)
+    print response_data
+    # access_token = response_data["access_token"]
+    # refresh_token = response_data["refresh_token"]
+    # token_type = response_data["token_type"]
+    # expires_in = response_data["expires_in"]
+    return redirect("http://www.google.com");
+
+@app.route("/convert")
+def convertPlaylist():
+    pass
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=PORT)
